d3f42f309939336bbfa1a82713ada593a9f57e65
Aditya Kappagantula <akappagantula@salesforce.com>
2023-09-21 21:08:26 -0700

    fix:added images

--- client/App/index.tsx
+++ client/App/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './styles.scss'
+// import './styles.scss'
 
 const App: React.FC = () => {
   const handleClick = () => {
@@ -11,6 +11,7 @@ const App: React.FC = () => {
   }
   return (
     <div className="body">
+      <img src="/assets/images/logo.png" alt="logo" />
       <h1>TypeScript is awesome!!</h1>
       <div>
         <input type="button" value="Get Name" onClick={handleClick} />
