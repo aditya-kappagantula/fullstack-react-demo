c4b8858f62d6f405472efc5ec3b680ee9703f253
Aditya Kappagantula <akappagantula@salesforce.com>
2023-09-25 14:36:40 -0700

    fix: updated jest

--- client/App/__tests__/index.tsx
+++ client/App/__tests__/index.tsx
@@ -1,11 +1,17 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import React from 'react'
 import App from '../index'
 
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({ test: 100 })
+  })
+) as jest.Mock
+
 describe('App tests', () => {
-  it('should contains the heading 1', () => {
+  it('should contains the heading 1', async () => {
     render(<App />)
-    const heading = screen.getByText(/Typescript is awesome!!/i)
-    expect(heading).not.toBeInTheDocument()
+    const heading = await waitFor(() => screen.getByText(/Energy Service App/i))
+    expect(heading).toBeInTheDocument()
   })
 })
--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,11 @@ module.exports = {
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  automock: false,
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  },
   transform: {
     '^.+\\.(t|j)sx?$': 'ts-jest'
   }
